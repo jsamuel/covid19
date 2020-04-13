f2dc36699b35bf50dc4e048a5e06c97bfcac75dd
Justin Samuel <js@justinsamuel.com>
2020-04-13 10:34:28 -0700

    example script to process COVID-19 data from Johns Hopkins

---
+++ example1.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+"""
+Reads and prints data from COVID-19 daily csv files from
+    https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports
+
+To use this script, first clone the COVID-19 data repo into the same directory
+as this script is in:
+
+    git clone https://github.com/CSSEGISandData/COVID-19
+
+Then run this script:
+
+    python3 ./example1.py --county Whatcom --state WA --country US
+
+If you've cloned the CSSEGISandData/COVID-19 repo to a different location than the
+same directory this script is in, you can set with the --data-repo-dir argument.
+"""
+import argparse
+import csv
+import glob
+
+
+def get_files_in_date_order(data_repo_dir):
+    reports_dir = data_repo_dir + '/csse_covid_19_data/csse_covid_19_daily_reports'
+
+    # TODO: don't ignore files before April.
+    # The files changed formats a few times. For quick implementation, I just
+    # ignored everything before April.
+    file_list = glob.glob(reports_dir + '/04-*-2020.csv')
+    file_list.sort()
+    return file_list
+
+
+def get_file_date(path):
+    filename = path.split('/')[-1]
+    before_dot = filename.split('.')[0]
+    return before_dot
+
+
+def get_records(combined_key, data_repo_dir):
+    records = []
+
+    prev_confirmed = 0
+    prev_deaths = 0
+    prev_recovered = 0
+    prev_active = 0
+
+    for filepath in get_files_in_date_order(data_repo_dir):
+        rec = {}
+        records.append(rec)
+
+        rec['date'] = get_file_date(filepath)
+
+        with open(filepath) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['Combined_Key'] != combined_key:
+                    continue
+
+                rec['confirmed'] = int(row['Confirmed'])
+                rec['deaths'] = int(row['Deaths'])
+                rec['recovered'] = int(row['Recovered'])
+                rec['active'] = int(row['Active'])
+
+                rec['new_confirmed'] = rec['confirmed'] - prev_confirmed
+                rec['new_deaths'] = rec['deaths'] - prev_deaths
+                rec['new_recovered'] = rec['recovered'] - prev_recovered
+                rec['new_active'] = rec['active'] - prev_active
+
+                prev_confirmed = rec['confirmed']
+                prev_deaths = rec['deaths']
+                prev_recovered = rec['recovered']
+                prev_active = rec['active']
+
+    return records
+
+
+def print_raw(records):
+    for record in records:
+        print(record)
+
+
+def print_histogram(records, key):
+    for rec in records:
+        print(rec['date'], end='')
+        print('\t', end='')
+        print('| ', end='')
+        print('#' * rec[key])
+
+
+def main(county, state, country, data_repo_dir):
+    combined_key = f'{county}, {state}, {country}'
+
+    print('*' * 80)
+    print(f'Location: {combined_key}')
+    print('*' * 80)
+
+    records = get_records(combined_key, data_repo_dir)
+
+    print()
+    print('Raw')
+    print('-' * 40)
+    print_raw(records)
+
+    print()
+    print('Confirmed')
+    print('-' * 40)
+    print_histogram(records, key='new_confirmed')
+
+    print()
+    print('Deaths')
+    print('-' * 40)
+    print_histogram(records, key='new_deaths')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--county', default='Whatcom')
+    parser.add_argument('--state', default='Washington')
+    parser.add_argument('--country', default='US')
+    parser.add_argument('--data-repo-dir', default='COVID-19')
+
+    args = parser.parse_args()
+    main(args.county, args.state, args.country, args.data_repo_dir)
